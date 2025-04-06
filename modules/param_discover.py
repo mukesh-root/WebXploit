2de19a91540c2ec7283bfcc79c090cf8506eda94
Xoscyber$ <xoscyber53@gmail.com>
2025-04-06 14:07:36 +0530

    Update param_discover.py

--- modules/param_discover.py
+++ modules/param_discover.py
@@ -1,1 +1,9 @@
-# param_discover.py - boilerplate
+import subprocess
+from utils.rich_logger import log_info, log_success
+
+def run(target):
+    log_info("Running ParamSpider...")
+    cmd = f"python3 ParamSpider/paramspider.py -d {target} -o outputs/parameters.txt"
+    subprocess.call(cmd, shell=True)
+    log_success("Parameter discovery complete.")
+
