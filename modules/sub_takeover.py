6187d7107d6f0b2f0fb012bef2c853a0b936c46b
Xoscyber$ <xoscyber53@gmail.com>
2025-04-06 14:10:41 +0530

    Update sub_takeover.py

--- modules/sub_takeover.py
+++ modules/sub_takeover.py
@@ -1,1 +1,9 @@
-# sub_takeover.py - boilerplate
+import subprocess
+from utils.rich_logger import log_info, log_success
+
+def run(subdomain_file):
+    log_info("Checking for subdomain takeover...")
+    cmd = f"subzy run --targets {subdomain_file} --hide_fails --output outputs/sub_takeover.txt"
+    subprocess.call(cmd, shell=True)
+    log_success("Subdomain takeover check complete.")
+
